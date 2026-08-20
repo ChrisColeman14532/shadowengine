3334998783bbf531b144881e69d4176e8445d73a
Chris Coleman <coleman14532@gmail.com>
2026-08-20 15:53:24 -0400

    fix: skybox VBO/EBO buffer separation fix

--- engine/src/engine.cpp
+++ engine/src/engine.cpp
@@ -959,15 +959,18 @@ void CoreEngine::InitSkybox() {
         20,21,22, 20,22,23  // left
     };
 
+    GLuint skyboxVBO, skyboxEBO;
     glGenVertexArrays(1, &s_skyboxVAO);
-    glGenBuffers(1, &s_skyboxVBO);
+    glGenBuffers(1, &skyboxVBO);
+    glGenBuffers(1, &skyboxEBO);
 
     glBindVertexArray(s_skyboxVAO);
-    glBindBuffer(GL_ARRAY_BUFFER, s_skyboxVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVerts), cubeVerts, GL_STATIC_DRAW);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (void*)0);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, s_skyboxVBO);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, skyboxEBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cubeIndices), cubeIndices, GL_STATIC_DRAW);
     glBindVertexArray(0);
 
